031e88551852d2a4429bef27c2d71201e76acb3f
NguyenHuuDai <nguyenhuudaihs@icloud.com>
2024-08-13 18:21:58 +0700

    app

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import "./styles/globals.css";
-import App from "./App";
+import App from "./App"; //update
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
